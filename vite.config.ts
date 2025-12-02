c1d64479fd945a85264ddab4db32276f7c69f241
nohyewon <yw4913@naver.com>
2025-12-03 00:39:01 +0900

    :hammer:[rename]: 폴더 절대경로

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,18 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  // 절대경로 설정
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@constants", replacement: "/src/constants" },
+      { find: "@hooks", replacement: "/src/hooks" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@services", replacement: "/src/services" },
+      { find: "@utils", replacement: "/src/utils" },
+    ],
+  },
 });
