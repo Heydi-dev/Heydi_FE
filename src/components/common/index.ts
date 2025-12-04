842d32e721ebdcd6a0cd2562979b33fa6aea79ed
nohyewon <yw4913@naver.com>
2025-12-05 00:34:32 +0900

    :sparkles:[feat]: Header 구현

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,1 +1,4 @@
-export { default as BottomNav } from "./BottomNav";
+// Header & Nav
+export { default as BottomNav } from "./Header&Nav/BottomNav";
+export { default as DefaultHeader } from "./Header&Nav/DefaultHeader";
+export { default as BackHeader } from "./Header&Nav/BackHeader";
