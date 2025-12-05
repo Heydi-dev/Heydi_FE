4f92f9ca6460edeea0065d3b32c2b44044ac4e4c
nohyewon <yw4913@naver.com>
2025-12-05 22:46:40 +0900

    :sparkles:[feat]: Login UI 구현

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { VscEyeClosed, VscEye } from "react-icons/vsc";
 
 interface InputProps {
-  label?: string;
   placeholder?: string;
   value?: string;
   // eslint-disable-next-line no-unused-vars
@@ -9,7 +9,7 @@ interface InputProps {
   error?: string;
   disabled?: boolean;
   className?: string;
-  type?: string;
+  type?: string; // "text" | "password"
 }
 
 const Input = ({
@@ -21,18 +21,24 @@ const Input = ({
   className = "",
   type = "text",
 }: InputProps) => {
+  const [showPw, setShowPw] = useState(false);
+
+  const isPassword = type === "password";
+  const inputType = isPassword && !showPw ? "password" : "text";
+
   return (
-    <div className={`w-full flex flex-col gap-1 ${className}`}>
+    <div className={`relative w-full flex flex-col gap-1 ${className}`}>
       <input
-        type={type}
+        type={inputType}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
         disabled={disabled}
+        spellCheck={false}
         className={`
-          w-full h-12 px-4
+          w-full h-12 px-4 pr-10
           rounded-[8px]
-          border text-sm outline-none   /* ← text-sm (14px) */
+          border text-sm outline-none
 
           placeholder-[#D0D0D0]
           ${error ? "text-[#E94235]" : "text-[#4A4A4A]"}
@@ -48,6 +54,22 @@ const Input = ({
           transition-none
         `}
       />
+
+      {isPassword && !disabled && (
+        <div
+          className="
+            absolute right-3 top-1/2 -translate-y-1/2 
+            cursor-pointer text-[#D9D9D9]
+          "
+          onClick={() => setShowPw(!showPw)}
+        >
+          {showPw ? (
+            <VscEye size={20} color="EFE8E1" />
+          ) : (
+            <VscEyeClosed size={20} />
+          )}
+        </div>
+      )}
     </div>
   );
 };
