140af3ec6f40cb791631fd90011be7f680e806b3
nohyewon <yw4913@naver.com>
2025-12-30 00:17:01 +0900

    :recycle:[refactor]: 리포트 더미데이터 적용

--- src/pages/report/Report.tsx
+++ src/pages/report/Report.tsx
@@ -9,7 +9,7 @@
  * - 인사이트 & 피드백
  * - 캘린더
  * - 한 달 전 하루 일기 카드
- * - 하드코딩 된 데이터 사용
+ * - 임시 더미 데이터 사용
  */
 
 import { useState } from "react";
@@ -25,11 +25,7 @@ import {
   MonthModal,
 } from "@components/index";
 import { IoCalendarNumberOutline } from "react-icons/io5";
-import {
-  EMOTION_CHART_DUMMY,
-  TOPICS_DUMMY,
-  CALENDAR_DUMMY,
-} from "@mocks/report";
+import { REPORT_DUMMY } from "@mocks/report";
 
 const Report = () => {
   const navigate = useNavigate();
@@ -59,12 +55,12 @@ const Report = () => {
             이번달의 감정 변화
           </p>
         </div>
-        <EmotionChart data={EMOTION_CHART_DUMMY} />
+        <EmotionChart data={REPORT_DUMMY.emotionChart} />
 
         <div className="w-full flex flex-col mb-3">
           <p className="text-base font-bold text-[#4A4A4A]">자주 나온 주제</p>
         </div>
-        <TopTopics topics={TOPICS_DUMMY} />
+        <TopTopics topics={REPORT_DUMMY.topTopics} />
 
         <div className="w-full mb-6">
           <div className="flex justify-between">
@@ -78,7 +74,7 @@ const Report = () => {
                   flex items-center justify-center
                 "
               >
-                맛집 탐방
+                {REPORT_DUMMY.likes}
               </div>
             </div>
 
@@ -92,7 +88,7 @@ const Report = () => {
                   flex items-center justify-center
                 "
               >
-                발표
+                {REPORT_DUMMY.dislikes}
               </div>
             </div>
           </div>
@@ -105,8 +101,7 @@ const Report = () => {
         </div>
         <div className="w-full bg-[#EFE8E1] rounded-xl p-4 mb-6">
           <p className="text-[10px] text-[#4A4A4A] leading-5">
-            이번 달에는 학교에 가서 친구들과 점심을 많이 먹었어요! 학교 식당에
-            가기도 하고 배달 음식을 시켜 먹으며 좋은 추억을 만들었습니다.
+            {REPORT_DUMMY.activitySummary}
           </p>
         </div>
 
@@ -117,8 +112,7 @@ const Report = () => {
         </div>
         <div className="w-full bg-[#EFE8E1] rounded-xl p-4 mb-6">
           <p className="text-[10px] leading-5 text-[#4A4A4A]">
-            운동을 한다고 했지만 계속 미루기만 했어요. 다음 달에는 누워있는
-            시간을 줄이고 잠깐이라도 운동을 하는 습관을 만들어보면 어떨까요?
+            {REPORT_DUMMY.insight}
           </p>
         </div>
 
@@ -126,7 +120,7 @@ const Report = () => {
           <p className="text-base font-bold text-[#4A4A4A]">캘린더</p>
         </div>
 
-        <Calendar year={year} month={month} calendars={CALENDAR_DUMMY} />
+        <Calendar year={year} month={month} calendars={REPORT_DUMMY.calendar} />
 
         <div className="w-full mt-6 mb-2">
           <p className="text-base font-bold text-[#4A4A4A] mb-2">
@@ -134,10 +128,10 @@ const Report = () => {
           </p>
 
           <DiaryCard
-            date="11월 20일"
-            emotion="짜증"
-            topic="친구"
-            onClick={() => navigate(`/diary/detail/:diaryId`)}
+            {...REPORT_DUMMY.lastMonthDiary}
+            onClick={() =>
+              navigate(`/diary/detail/${REPORT_DUMMY.lastMonthDiary.diaryId}`)
+            }
           />
         </div>
       </Container>
